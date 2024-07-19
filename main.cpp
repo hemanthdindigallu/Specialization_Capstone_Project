8fa974786dcd8a8a4ad98b4b7dddf893d6736c7f
Hemanth <130672795+hemanthdindigallu@users.noreply.github.com>
2024-07-19 21:12:48 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -8,12 +8,37 @@
 using namespace std;
 namespace fs = std::filesystem;
 
+__host__ cudnnHandle_t createCudaHandleAndOutputHWSpecs()
+{
+    return 0;
+}
+
+__host__ std::tuple<cudnnTensorDescriptor_t, float*, int, int, int> loadImageAndPreprocess(const char* filePath)
+{
+    
+    return 0;
+}
+
+__host__ float* runCuDnnModel()
+{
+    return 0;
+}
+
+__host__ void printClassificationResults()
+{
+    return 0;
+}
 
 int main()
 {
+    if (argc < 3) {
+        cerr << "Usage: " << argv[0] << " <input_folder> <output_file.txt>" << endl;
+        return 1;
+    }
+
     const string input_folder = argv[1];
     const string output_file_path = argv[2];
-    int num_classes = 10; 
+    int num_classes = 10; // Example number of classes
 
     cudnnHandle_t handle_ = createCudaHandleAndOutputHWSpecs();
     ofstream output_file(output_file_path);
